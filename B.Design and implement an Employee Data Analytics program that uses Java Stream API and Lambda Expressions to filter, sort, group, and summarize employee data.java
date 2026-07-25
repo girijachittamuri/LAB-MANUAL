83f8b6abbe63202dcc60eb2b0815e334cbda6a29
girijachittamuri <girijachittamuri@gmail.com>
2026-07-25 11:36:21 +0530

    B.Design and implement an Employee Data Analytics program that uses Java Stream API and Lambda Expressions to filter, sort, group, and summarize employee data.java

---
+++ B.Design and implement an Employee Data Analytics program that uses Java Stream API and Lambda Expressions to filter, sort, group, and summarize employee data.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Employee {
+
+    public static void main(String[] args) {
+
+        List<String> emp = Arrays.asList(
+                "Girija", "Ram", "Kiran");
+
+        for (String e : emp) {
+            System.out.println(e);
+        }
+    }
+}
