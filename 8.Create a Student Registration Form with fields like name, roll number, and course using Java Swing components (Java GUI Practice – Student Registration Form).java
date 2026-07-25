4f0046ebb00840fd138760f79d84d905a8714cd3
girijachittamuri <girijachittamuri@gmail.com>
2026-07-25 09:12:33 +0530

    8.Create a Student Registration Form with fields like name, roll number, and course using Java Swing components (Java GUI Practice – Student Registration Form).java

---
+++ 8.Create a Student Registration Form with fields like name, roll number, and course using Java Swing components (Java GUI Practice – Student Registration Form).java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+public class StudentForm {
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame("Student Form");
+
+        JLabel l1 = new JLabel("Name");
+        JLabel l2 = new JLabel("Roll No");
+        JLabel l3 = new JLabel("Course");
+
+        JTextField t1 = new JTextField();
+        JTextField t2 = new JTextField();
+        JTextField t3 = new JTextField();
+
+        JButton b = new JButton("Submit");
+
+        l1.setBounds(50,50,100,30);
+        t1.setBounds(150,50,100,30);
+
+        l2.setBounds(50,100,100,30);
+        t2.setBounds(150,100,100,30);
+
+        l3.setBounds(50,150,100,30);
+        t3.setBounds(150,150,100,30);
+
+        b.setBounds(100,200,100,30);
+
+        f.add(l1); f.add(t1);
+        f.add(l2); f.add(t2);
+        f.add(l3); f.add(t3);
+        f.add(b);
+
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
