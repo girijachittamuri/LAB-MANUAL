75c20a01beda6c8e04121ef1cf77532e19a1f64f
girijachittamuri <girijachittamuri@gmail.com>
2026-07-25 11:34:31 +0530

    A.Design and implement a Chess Game using Object-Oriented Programming principles such as inheritance, polymorphism, and encapsulation to model pieces, moves, and game rules.java

---
+++ A.Design and implement a Chess Game using Object-Oriented Programming principles such as inheritance, polymorphism, and encapsulation to model pieces, moves, and game rules.java
@@ -0,0 +1,11 @@
+class Chess {
+
+    void move() {
+        System.out.println("Chess piece moved.");
+    }
+
+    public static void main(String[] args) {
+        Chess c = new Chess();
+        c.move();
+    }
+}
